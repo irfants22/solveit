ced945e3f1e848ebb86273923f7d6ae5af96bfc0
irfants22 <irfanee02@gmail.com>
2025-11-13 23:59:30 +0700

    refactor: rename bio column to biodata in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('bio')->nullable();
+            $table->text('biodata')->nullable();
             $table->integer('age')->nullable();
             $table->string('address')->nullable();
             $table->rememberToken();
